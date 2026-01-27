aa2239bd4865662fa1c9b37b441528465371c137
kanhaiya1202 <kanhaiyasharma1202@gamil.com>
2026-01-27 17:10:30 +0530

    create the destructure

--- day4/app.js
+++ day4/app.js
@@ -1,12 +1,11 @@
-const operation =  require('./calculate.js');
+const {subtraction,addtion,multiplication} =  require('./calculate.js');
+//{subtraction,addtion,multiplication} => this is know as destructure 
 
 var a = 10 ;
 var b = 20;
 console.log(`subtraction a:- ${a} and b:-${b}`) 
-operation.subtraction(a,b)
+subtraction(a,b)
 console.log(`Multiplication a:- ${a} and b:-  ${b}`)
-operation.multiplication(a,b)
+multiplication(a,b)
 console.log(`Additon a:- ${a} and b:- ${b}`)
-operation.addtion(a,b)
-
-
+addtion(a,b)
